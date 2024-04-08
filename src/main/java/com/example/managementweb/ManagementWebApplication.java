86ba2ad1d1903976b665f098b1e3d13286313e7a
SangNguyen <nngocsang38@gmail.com>
2024-04-08 15:48:01 +0700

    first

---
+++ src/main/java/com/example/managementweb/ManagementWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.managementweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/managementweb/ManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managementweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
