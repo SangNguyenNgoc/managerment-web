9b31e93a67329721111e22fac1d22956c50bf6cd
Anhdung2k3VN <doananhdung.work@gmail.com>
2024-04-20 19:05:31 +0700

    push

--- src/main/java/com/example/managementweb/models/dtos/penalize/PenalizeCreateDto.java
+++ src/main/java/com/example/managementweb/models/dtos/penalize/PenalizeCreateDto.java
@@ -1,2 +1,34 @@
-package com.example.managementweb.models.dtos.penalize;public class PenalizeCreateDto {
+package com.example.managementweb.models.dtos.penalize;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.example.managementweb.models.entities.PenalizeEntity}
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PenalizeCreateDto {
+    @NotBlank(message = "Mã xử lí không được bỏ trống")
+    @Pattern(regexp = "\\d", message = "Mã xử lí chỉ được chứa ký tự số")
+    String id;
+    @NotBlank(message = "Mã thành viên không được bỏ trống")
+    @Pattern(regexp = "\\d", message = "Mã thành viên chỉ được chứa ký tự số")
+    String person;
+    @NotBlank(message = "Hình thức xử lí không được để trống")
+    String type;
+    @Pattern(regexp = "\\d", message = "Tiền chỉ được chứa ký tự số")
+    String payment;
+//    @NotBlank(message = "Ngày xử lí không được để trống")
+//    String date;
+//    @NotBlank(message = "Trạng thái xử lí không được để trống")
+//    String status;
 }
--- src/main/java/com/example/managementweb/models/dtos/penalize/PenalizeResponseDto.java
+++ src/main/java/com/example/managementweb/models/dtos/penalize/PenalizeResponseDto.java
@@ -1,2 +1,31 @@
-package com.example.managementweb.models.dtos.penalize;public class PenalizeResponseDto {
+package com.example.managementweb.models.dtos.penalize;
+
+
+import com.example.managementweb.models.entities.PenalizeEntity;
+import com.example.managementweb.models.entities.PersonEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link PenalizeEntity}
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class PenalizeResponseDto  implements Serializable {
+    Long id;
+    String type;
+    Integer payment;
+    LocalDateTime date;
+    Boolean status;
+    PersonEntity person;
+
 }
